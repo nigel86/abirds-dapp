d0015710385a221afc7548a73d1de31a304ad59e
Nigel Chang <nigel86@gmail.com>
2023-08-12 11:57:45 +0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/abirds-dapp/",
   plugins: [react(), nodePolyfills()],
   define: {
     "process.env": {},
