04ee5dd9ec48866d01a5411eee67c37496070b58
nigel86 <nigel86@users.noreply.github.com>
2023-12-14 16:30:21 +0000

    deploy: 5428aa99ff17ff0aa6806ef3e3ab7b6dc1d241fc

---
+++ assets/hasERC20Allowance-1499f181.browser.esm.e41b7091.js
@@ -0,0 +1,1 @@
+import{C as d,ag as i}from"./index.b79cd932.js";async function c(a,e,s){const n=a.getProvider(),r=new d(n,e,i,{},a.storage),t=await a.getSignerAddress(),o=a.address;return(await r.read("allowance",[t,o])).gte(s)}export{c as h};
